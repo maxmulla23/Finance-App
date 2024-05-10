a74feccf0a17e2e6930c1fcdd723f4e788070e9b
maxmulla23 <maxmulla23@gmail.com>
2024-05-10 17:23:04 +0300

    controllers configured

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e8d567596820c77efba2d683293c793b7f8d1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bfcec4bd2793c916f48d87bf607552e9e069a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
