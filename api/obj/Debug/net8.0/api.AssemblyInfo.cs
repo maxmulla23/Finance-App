649f4f59c86c5eff2a6c02bbec77306b20693f19
maxmulla23 <maxmulla23@gmail.com>
2024-05-14 20:46:17 +0300

    config

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6771322d6af4e89098ae44d9ce40e5c614fbd2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97bfc0d4909e044b000af21ea89199fad16595ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
