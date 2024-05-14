e59e808befb16bbb684391205eb1ce70041cfe18
maxmulla23 <maxmulla23@gmail.com>
2024-05-14 20:46:11 +0300

    update comment

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -37,5 +37,22 @@ namespace api.Repository
         {
            return await _context.Comments.FindAsync(id);
         }
+
+        public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
+        {
+            var existingcomment = await _context.Comments.FindAsync(id);
+
+            if(existingcomment == null)
+            {
+                return null;
+            }
+
+            existingcomment.Title = commentModel.Title;
+            existingcomment.Content = commentModel.Content;
+
+            await _context.SaveChangesAsync();
+
+            return existingcomment;
+        }
     }
 }
