64cfa7b86950f23918a3f06fe21cf16c0d7610cd
maxmulla23 <maxmulla23@gmail.com>
2024-05-14 20:22:45 +0300

    create async

--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -20,5 +20,16 @@ namespace api.Mappers
                 StockId = commentModel.StockId
             };
         }
+
+        public static Comment ToCommentFromCreate(this CreateCommentDto commentDto, int stockId)
+        {
+            return new Comment
+            {
+                
+                Title = commentDto.Title,
+                Content = commentDto.Content,
+                StockId = stockId
+            };
+        }
     }
 }
