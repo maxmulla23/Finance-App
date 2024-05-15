82cd4be7622274ca86cbd14a556efb093a587675
maxmulla23 <maxmulla23@gmail.com>
2024-05-15 19:49:31 +0300

    sort by descending order using symbol

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -54,6 +54,14 @@ namespace api.Repository
                 stocks = stocks.Where(s => s.Symbol.Contains(queryObject.Symbol));
             }
 
+            if(!string.IsNullOrWhiteSpace(queryObject.SortBy))
+            {
+                if(queryObject.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = queryObject.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
             return await stocks.ToListAsync();
         }
 
