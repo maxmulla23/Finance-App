4cb23a4c9cb56e144b85d329277e7cb6cd523752
maxmulla23 <maxmulla23@gmail.com>
2024-05-15 19:07:48 +0300

    data annotations

--- api/Dtos/Stock/Comment/CommentDto.cs
+++ api/Dtos/Stock/Comment/CommentDto.cs
@@ -7,7 +7,7 @@ namespace api.Dtos.Stock.Comment
 {
     public class CommentDto
     {
-          public int Id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
