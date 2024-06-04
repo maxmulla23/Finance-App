cd53b9ae0ab43153da6168c3badfb32b37673f9f
maxmulla23 <maxmulla23@gmail.com>
2024-06-04 16:39:25 +0300

    deleted migrations

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82cd4be7622274ca86cbd14a556efb093a587675")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e0e24939a1476030bf71127d1cfb328fe406c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
