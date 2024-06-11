3c078ac5799c64ce8bde133c353f78e90bcee357
maxmulla23 <maxmulla23@gmail.com>
2024-06-11 13:13:10 +0300

    identity user

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a854ae32a478df44b6f946c7d5ed877cfc484934")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f83c130d01a125823faef40fd73dc8a7400a9ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
