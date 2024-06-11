a854ae32a478df44b6f946c7d5ed877cfc484934
maxmulla23 <maxmulla23@gmail.com>
2024-06-11 12:05:08 +0300

    added identity pckages

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd53b9ae0ab43153da6168c3badfb32b37673f9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e02b68159bb99b68b8937214f2032cba38c6bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
