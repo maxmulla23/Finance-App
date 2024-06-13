7d2b67a4d3fb8628c710c01a56cfbb704bd0f8f3
maxmulla23 <maxmulla23@gmail.com>
2024-06-13 12:40:12 +0300

    portforlio repository

--- api/Program.cs
+++ api/Program.cs
@@ -92,6 +92,7 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<Iportfoliorepository, PortfolioRepository>();
 
 var app = builder.Build();
 
