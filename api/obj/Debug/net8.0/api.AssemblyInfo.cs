39a71eed903f4575e97cf0405dff55f0c74abd9e
maxmulla23 <maxmulla23@gmail.com>
2024-06-13 12:41:50 +0300

    portfolio class created and completed

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb8dee50d38741943361eea2b22b3c774518c00e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69eaec32eb922c89f81da0419c81a4557b71b95")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
