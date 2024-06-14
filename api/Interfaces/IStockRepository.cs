69c9aa4e8737e0ea6ed13faf200e5753e827525b
maxmulla23 <maxmulla23@gmail.com>
2024-06-14 11:43:53 +0300

    find by symbol

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -12,6 +12,7 @@ namespace api.Interfaces
     {
         Task<List<Stock>> GetAllAsync(QueryObject queryObject);
         Task<Stock?> GetByIdAsync(int id); //firstordefault can be null
+        Task<Stock?> GetBySymbolAsync(string symbol);
         Task<Stock> CreateAsync(Stock stockModel);
         Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
         Task<Stock?> DeleteAsync(int id);
