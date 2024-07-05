07cf1d6ee81d02d4aad3bd47549a68f73636b2bb
maxmulla23 <maxmulla23@gmail.com>
2024-07-05 03:01:56 +0300

    props

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import "./Card.css";
 
-type Props = {}
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
 
-const Card = (props: Props) => {
+const Card: React.FC<Props> = ({ companyName, ticker, price }: Props) : JSX.Element => {
   return (
-    <div>Card</div>
+    <div className='card'>
+      Card
+      <div className='details'>
+        <h2>
+          {companyName} ({ticker})
+        </h2>
+        <p>{price}</p>
+      </div>
+      <p className='info'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Et saepe aut voluptatum beatae dicta nulla minus assumenda eius, 
+        illo amet quia cumque quo mollitia nostrum, accusamus tempore veritatis modi optio.
+      </p>
+      </div>
   )
 }
 
