1eeaa3ffe6f12f02e04e34b920f200b7b4cc6297
maxmulla23 <maxmulla23@gmail.com>
2024-07-18 17:36:16 +0300

    s

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,44 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
   const [search, setSearch] = useState("")
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([])
+  const [serverError, setServerError] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
       setSearch(e.target.value);
       console.log(e);
       
   }
-  const onClick = (e: SyntheticEvent) => {
+
+  const onPortfolioCreate = (e: SyntheticEvent) => {
+    e.preventDefault();
     console.log(e);
+    
+  }
+
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
+    
     
   }
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange} />
-      <CardList />
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate} />
+      {serverError && <div>Unable to connect to API</div>}
+      
     </div>
   );
 }
--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import Card from '../Card/Card'
+import { CompanySearch } from '../../company'
+import { v4 as uuidv4 } from "uuid"
 
-interface Props {}
 
-const CardList : React.FC<Props> = (props: Props) : JSX.Element => {
+interface Props {
+  searchResults: CompanySearch[];
+  onPortfolioCreate: (e: SyntheticEvent) => void;
+}
+
+const CardList : React.FC<Props> = ({searchResults, onPortfolioCreate}: Props) : JSX.Element => {
   return (
-    <div>
-            <Card companyName='Apple' ticker='AApl' price={100}/>
-            <Card companyName='Safaricom' ticker='SAF' price={85}/>
-            <Card companyName='Microsoft' ticker='MSFT' price={120}/>
-
-    </div>
+    <>
+    {searchResults.length > 0 ? (
+      searchResults.map((result) => {
+        return <Card id={result.symbol} key={uuidv4()} searchResult={result} onPortfolioCreate={onPortfolioCreate} />
+      })
+    ) : (
+      <h1>No Results</h1>
+    ) };
+    </>
   )
 }
 
