f9a8cfce9cfca0974d0d9f28d2a25435aa6a0435
maxmulla23 <maxmulla23@gmail.com>
2024-07-23 15:27:48 +0300

    financial modelling prep api

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,14 +1,14 @@
 import axios from "axios"
 import { CompanySearch } from "./company"
 
-interface SearchResponse {
+export interface SearchResponse {
     data: CompanySearch[]
 }
 
 export const searchCompanies = async (query: string) => {
     try {
         const data = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/api/v3/search-name?query=META&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_KEY}`
+            `https://financialmodelingprep.com/api/v3/search-name?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
         )
         return data
     } catch (error) {
