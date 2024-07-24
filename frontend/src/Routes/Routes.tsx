155002415b3c8f4648854c0287bea25e43036649
maxmulla23 <maxmulla23@gmail.com>
2024-07-24 12:38:47 +0300

    balance sheet

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -6,6 +6,7 @@ import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 import DesignPage from "../Pages/DesignPage/DesignPage";
+import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
 
 export const router = createBrowserRouter([
     {
@@ -18,7 +19,8 @@ export const router = createBrowserRouter([
             {path: "company/:ticker", element: <CompanyPage />,
                 children: [
                     {path: "company-profile", element: <CompanyProfile /> },
-                    {path: "income-statement", element: <IncomeStatement /> }
+                    {path: "income-statement", element: <IncomeStatement /> },
+                    {path: "balance-sheet", element: <BalanceSheet /> }
                 ]
              },
             
