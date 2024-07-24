cbc9b6a90499a1323d194bea419d68fb19fbc309
maxmulla23 <maxmulla23@gmail.com>
2024-07-24 12:06:41 +0300

    income statement

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -20,7 +20,7 @@ const DesignPage = (props: Props) => {
         <h1>Finshark Design Page</h1>
         <h2>This is where we will house various design aspects of the app</h2>
         <RatioList data={testIncomeStatementData} config={tableConfig} />
-        <Table />
+        <Table data={testIncomeStatementData} config={tableConfig} />
     </>
   )
 }
