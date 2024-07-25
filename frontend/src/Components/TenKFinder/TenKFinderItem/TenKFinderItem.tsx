57a4b0632b6c1d661ea5a0700296b1dc1efe1169
maxmulla23 <maxmulla23@gmail.com>
2024-07-25 15:16:40 +0300

    tenK finder

---
+++ frontend/src/Components/TenKFinder/TenKFinderItem/TenKFinderItem.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { CompanyTenK } from '../../../company'
+import { Link } from 'react-router-dom';
+
+type Props = {
+    tenK: CompanyTenK;
+}
+
+const TenKFinderItem = ({tenK}: Props) => {
+    const fillingDate = new Date(tenK.fillingDate).getFullYear()
+  return (
+    <Link
+        reloadDocument
+        to={tenK.finalLink}
+        type="button"
+        className="iniline-flex items-center p-4 text-md text-white bg-lightGreen rounded-md"
+    >10K - {tenK.symbol} - {fillingDate} </Link>
+  )
+}
+
+export default TenKFinderItem
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CompanyBalanceSheet, CompanyCashFlow, CompanyCompData, CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company"
+import { CompanyBalanceSheet, CompanyCashFlow, CompanyCompData, CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch, CompanyTenK } from "./company"
 
 export interface SearchResponse {
     data: CompanySearch[]
@@ -90,6 +90,18 @@ export const getCompData = async (query: string) => {
     try {
         const data = await axios.get<CompanyCompData[]>(
             `https://financialmodellingprep.com/api/v4/stock_peers?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+        )
+        return data;
+    } catch (error: any) {
+        console.log("Error message from API: ", error.message);
+        
+    }
+}
+
+export const getTenK = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyTenK[]>(
+            `https://financialmodelingprep.com/api/v3/sec_filings/${query}?type=10-k&page=0&apikey=${process.env.REACT_APP_API_KEY}`
         )
         return data;
     } catch (error: any) {
