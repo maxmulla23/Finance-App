afdf38b3439930de4df693432933443d5c31a3f4
maxmulla23 <maxmulla23@gmail.com>
2024-07-29 08:06:38 +0300

    cors

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7969d92ff1e2180eb82cc0f18a7727377720ba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11f212b2bc9f5458000ac41e906150ddef527c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
