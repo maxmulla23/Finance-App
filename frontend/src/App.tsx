032c13449cd0e8948e610444bcb16fb762067f66
maxmulla23 <maxmulla23@gmail.com>
2024-08-03 14:50:47 +0300

    context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import "react-toastify/dist/react-toastify.css"
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { ToastContainer } from 'react-toastify';
+import { UserProvider } from './Context/useAuth';
 
 
 function App() {
 
   return (
     <>
+      <UserProvider>
       <Navbar />
       <Outlet />
       <ToastContainer />
+      </UserProvider>
     </>
   );
 }
