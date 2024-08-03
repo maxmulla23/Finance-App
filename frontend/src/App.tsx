40c47af459f5f8f48cc2ae5d508924b712d4d4aa
maxmulla23 <maxmulla23@gmail.com>
2024-08-03 13:37:56 +0300

    auth service

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router';
+import "react-toastify/dist/react-toastify.css"
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
